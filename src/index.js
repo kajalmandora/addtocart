7d80593a556c6a5b7bb2b8fff8000580cab2afba
kajalmandora <kajalmandora345@gmail.com>
2022-09-24 14:28:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddToCart from './components/addtocart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <AddToCart />
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
